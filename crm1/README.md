144e24dccbdbf0b22fbdee0970b25523b7f46656
Koketso motse <koketsomotse@gmail.com>
2021-10-15 14:23:03 +0200

    final version

---
+++ crm1/README.md
@@ -0,0 +1,22 @@
+# crash-course-CRM
+Django project with mysql database connection
+
+#pip modules to install
+
+pip install django-filter
+pip install mysql
+pip install django-widget-tweaks
+
+
+#to run the project:
+
+open the folder, terminal and type:
+
+python manager.py runserver
+
+
+
+#admin passwords
+
+username: admin
+password: admin
--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -1,49 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-
-class Task(models.Model):
-
-    STATUS = (
-        ('Pending', 'Pending'),
-        ('Out for delivery', 'Out for delivery'),
-        ('Delivered', 'Delivered'),
-    )
-    taskname = models.CharField(max_length=200)
-    taskdescription = models.CharField(max_length=200)
-    taskfeatures = models.CharField(max_length=200)
-    date_created = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=200, choices=STATUS)
-
-    def __str__(self):
-        return self.taskname
-
-
 class Customer(models.Model):
-    id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    repassword = models.CharField(max_length=200)
-    fristname = models.CharField(max_length=200)
-    surname = models.CharField(max_length=200)
-    cellnumber = models.IntegerField()
-    email = models.CharField(max_length=200)
-    address = models.CharField(max_length=200)
-    time_added = models.DateTimeField(auto_now_add=True)
+	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+	name = models.CharField(max_length=200, null=True)
+	surname = models.CharField(max_length=200, null=True)
+	phone = models.CharField(max_length=200, null=True)
+	email = models.CharField(max_length=200, null=True)
+	password1 = models.CharField(max_length=200, null=True)
+	password2 = models.CharField(max_length=200, null=True)
+	profile_pic = models.ImageField(default="profile1.png", null=True, blank=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
-    def __str__(self):
-        return self.username
+	def __str__(self):
+		return self.name
 
+
+class Tag(models.Model):
+	name = models.CharField(max_length=200, null=True)
+
+	def __str__(self):
+		return self.name
+
+class Product(models.Model):
+	CATEGORY = (
+			('Indoor', 'Indoor'),
+			('Out Door', 'Out Door'),
+			)
+
+	name = models.CharField(max_length=200, null=True)
+	price = models.FloatField(null=True)
+	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+	description = models.CharField(max_length=200, null=True, blank=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	tags = models.ManyToManyField(Tag)
+
+	def __str__(self):
+		return self.name
 
 class Order(models.Model):
-    STATUS = (
-        ('Pending', 'Pending'),
-        ('Out for delivery', 'Out for delivery'),
-        ('Delivered', 'Delivered'),
-    )
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
-    status = models.CharField(max_length=200, null=True, choices=STATUS)
-    customer = models.ForeignKey(
-        Customer, null=True, on_delete=models.SET_NULL)
-    task = models.ForeignKey(Task, null=True, on_delete=models.SET_NULL)
+	STATUS = (
+			('Pending', 'Pending'),
+			('Out for delivery', 'Out for delivery'),
+			('Delivered', 'Delivered'),
+			)
+
+	customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	status = models.CharField(max_length=200, null=True, choices=STATUS)
+	note = models.CharField(max_length=1000, null=True)
+
+	def __str__(self):
+		return self.product.name
+
+
+
+
